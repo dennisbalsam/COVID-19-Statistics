0fd082475008bbaffc79f9b6487fc25a4dbaadbe
Dennis Krupitsky <d.krupitsky@gmail.com>
2020-04-13 20:08:41 -0400

    added chart using react-chart-js2

--- src/App.js
+++ src/App.js
@@ -24,8 +24,8 @@ class App extends Component {
         return (
             <div className={styles.container}>
                 <Cards data={data} />
-                {/* <Countries />
-                <Chart/> */}
+                <Countries />
+                <Chart/>
             </div>
         )
     }
--- src/components/Chart/chart.jsx
+++ src/components/Chart/chart.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import { fetchDailyData } from '../../api'
+import {Line,Bar} from 'react-chartjs-2'
+import styles from './chart.module.css'
 
+const Chart = () => {
+    const [dailyData, setDailyData] = useState([]);
 
+    useEffect(() => {
+        const fetchAPI = async () => {
+            setDailyData(await fetchDailyData());
+        }
 
-export default function cards() {
+        console.log(dailyData);
+
+        fetchAPI();
+    });
+
+    const lineChart = (
+        dailyData.length
+        ? (<Line data ={{
+            labels: dailyData.map(({ date }) => date),
+            datasets: [
+                {data : dailyData.map(({ confirmed }) => confirmed), label: 'Infected', borderColor:'blue', fill: true},
+                {data : dailyData.map(({ deaths }) => deaths), label: 'Deaths', borderColor:'red', backgroundColor:'rgba(255,0,0,0.50', fill: true}
+            ]
+        }}/>): null 
+    );
+
     return (
-        <h1>chart</h1>
+        <div className={styles.container}>
+            {lineChart}
+        </div>
     )
 }
+
+export default Chart
